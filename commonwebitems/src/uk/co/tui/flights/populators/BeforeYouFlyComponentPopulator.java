e43f0bdb0b9372b41dacc0ee157c52cee387428c
vijaputh <vijaputh>
2015-09-25 07:37:34 +0000

    In-Progress US10001 : Merging Delta changes from CMC_FO branch to 16.2.0.0 branch

--- commonwebitems/src/uk/co/tui/flights/populators/BeforeYouFlyComponentPopulator.java
+++ commonwebitems/src/uk/co/tui/flights/populators/BeforeYouFlyComponentPopulator.java
@@ -34,11 +34,12 @@ import javax.annotation.Resource;
 import org.apache.commons.collections.CollectionUtils;
 import org.springframework.util.Assert;
 
+import uk.co.portaltech.travel.model.ContentValueModel;
 import uk.co.portaltech.tui.web.view.data.MediaViewData;
 import uk.co.tui.flights.data.ContentValueData;
 import uk.co.tui.flights.data.MediaData;
 import uk.co.tui.flights.web.view.data.BeforeYouFlyViewData;
-import uk.co.portaltech.travel.model.ContentValueModel;
+
 
 /**
  *
@@ -48,89 +49,95 @@ public class BeforeYouFlyComponentPopulator implements Populator<List<ContentVal
 
 
 
-   private static final String POPULATOR_NULL_ERROR = "Converter sources must not be null";
+	private static final String POPULATOR_NULL_ERROR = "Converter sources must not be null";
 
-   @Resource
-   private uk.co.tui.flights.populators.ContentValuePopulator contentValuePopulator;
+	@Resource
+	private uk.co.tui.flights.populators.ContentValuePopulator contentValuePopulator;
 
-   /**
-    * @description creates target of BeforeYouFlyListViewData
-    */
-   public BeforeYouFlyViewData createTarget()
-   {
-      return new BeforeYouFlyViewData();
-   }
+	/**
+	 * @description creates target of BeforeYouFlyListViewData
+	 */
+	public BeforeYouFlyViewData createTarget()
+	{
+		return new BeforeYouFlyViewData();
+	}
 
-   /**
-    * @param target
-    * @param source
-    * @description populates BeforeYouFlyListViewData
-    */
-   @Override
-   public void populate(final List<ContentValueModel> source, final BeforeYouFlyViewData target)
-   {
-      Assert.notNull(source, POPULATOR_NULL_ERROR);
-      Assert.notNull(target, POPULATOR_NULL_ERROR);
+	/**
+	 * @param target
+	 * @param source
+	 * @description populates BeforeYouFlyListViewData
+	 */
+	@Override
+	public void populate(final List<ContentValueModel> source, final BeforeYouFlyViewData target)
+	{
+		Assert.notNull(source, POPULATOR_NULL_ERROR);
+		Assert.notNull(target, POPULATOR_NULL_ERROR);
 
 
-      if (CollectionUtils.isNotEmpty(source))
-      {
-         getDataFromContentValue(target, source);
-      }
+		if (CollectionUtils.isNotEmpty(source))
+		{
+			getDataFromContentValue(target, source);
+		}
 
-   }
+	}
 
-   private void getDataFromContentValue(final BeforeYouFlyViewData target, final List<ContentValueModel> list)
-   {
-      if (CollectionUtils.isNotEmpty(list))
-      {
-         for (final ContentValueModel contentValueModel : list)
-         {
-            final ContentValueData contentValueData = new ContentValueData();
-            contentValuePopulator.populate(contentValueModel, contentValueData);
-            populateDescNImage(target, contentValueData);
-         }
-      }
-   }
+	private void getDataFromContentValue(final BeforeYouFlyViewData target, final List<ContentValueModel> list)
+	{
+		if (CollectionUtils.isNotEmpty(list))
+		{
+			for (final ContentValueModel contentValueModel : list)
+			{
+				final ContentValueData contentValueData = new ContentValueData();
+				contentValuePopulator.populate(contentValueModel, contentValueData);
+				populateDescNImage(target, contentValueData);
+			}
+		}
+	}
 
-   private void populateDescNImage(final BeforeYouFlyViewData target, final ContentValueData contentValueData)
-   {
-      if (target.getNames().get(0).equals(contentValueData.getName()))
-      {
-         target.setDisplayName(contentValueData.getValue());
-      }
-      else if (target.getNames().get(1).equals(contentValueData.getName()))
-      {
-         target.setIntroduction(contentValueData.getValue());
-         target.setGalleryImages(convertMediaToMediaViewData(contentValueData.getGalleryImages()));
-      }
-   }
+	private void populateDescNImage(final BeforeYouFlyViewData target, final ContentValueData contentValueData)
+	{
+		if (target.getNames().get(0).equals(contentValueData.getName()))
+		{
+			target.setDisplayName(contentValueData.getValue());
+		}
+		else if (target.getNames().get(1).equals(contentValueData.getName()))
+		{
+			target.setIntroduction(contentValueData.getValue());
+			target.setGalleryImages(convertMediaToMediaViewData(contentValueData.getGalleryImages()));
+		}
+		else if (target.getNames().get(2).equals(contentValueData.getName()))
+		{
+			target.setKeyFacts(contentValueData.getValue());
 
-   /**
-    * @description Use this method to populate the MediaViewData from MediaData which is a business
-    *
-    *
-    * @param mediaDatas
-    * @return List<MediaViewData>
-    */
-   private List<MediaViewData> convertMediaToMediaViewData(final Collection<MediaData> mediaDatas)
-   {
-      final List<MediaViewData> mediaViewDatas = new ArrayList<MediaViewData>();
-      if (isNotEmpty(mediaDatas))
-      {
-         for (final MediaData mediaData : mediaDatas)
-         {
-            final MediaViewData mediaViewData = new MediaViewData();
-            mediaViewData.setAltText(mediaData.getAltText());
-            mediaViewData.setCode(mediaData.getCode());
-            mediaViewData.setDescription(mediaData.getDescription());
-            mediaViewData.setMainSrc(mediaData.getUrl());
-            mediaViewData.setMime(mediaData.getMime());
-            mediaViewData.setSize(mediaData.getSize());
-            mediaViewDatas.add(mediaViewData);
-         }
-      }
-      return mediaViewDatas;
-   }
+		}
+
+	}
+
+	/**
+	 * @description Use this method to populate the MediaViewData from MediaData which is a business
+	 *
+	 *
+	 * @param mediaDatas
+	 * @return List<MediaViewData>
+	 */
+	private List<MediaViewData> convertMediaToMediaViewData(final Collection<MediaData> mediaDatas)
+	{
+		final List<MediaViewData> mediaViewDatas = new ArrayList<MediaViewData>();
+		if (isNotEmpty(mediaDatas))
+		{
+			for (final MediaData mediaData : mediaDatas)
+			{
+				final MediaViewData mediaViewData = new MediaViewData();
+				mediaViewData.setAltText(mediaData.getAltText());
+				mediaViewData.setCode(mediaData.getCode());
+				mediaViewData.setDescription(mediaData.getDescription());
+				mediaViewData.setMainSrc(mediaData.getUrl());
+				mediaViewData.setMime(mediaData.getMime());
+				mediaViewData.setSize(mediaData.getSize());
+				mediaViewDatas.add(mediaViewData);
+			}
+		}
+		return mediaViewDatas;
+	}
 
 }
--- commonwebitems/src/uk/co/tui/flights/web/view/data/BeforeYouFlyViewData.java
+++ commonwebitems/src/uk/co/tui/flights/web/view/data/BeforeYouFlyViewData.java
@@ -3,12 +3,13 @@
  */
 package uk.co.tui.flights.web.view.data;
 
-import uk.co.portaltech.tui.web.view.data.MediaViewData;
-
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+
+import uk.co.portaltech.tui.web.view.data.MediaViewData;
+
 
 /**
  * @author vijaya.putheti
@@ -17,133 +18,177 @@ import java.util.Map;
 public class BeforeYouFlyViewData
 {
 
-   private String displayName;
+	private String displayName;
 
-   private String introduction;
+	private String introduction;
 
-   private List<MediaViewData> galleryImages = new ArrayList<MediaViewData>();
+	private List<MediaViewData> galleryImages = new ArrayList<MediaViewData>();
 
-   private List<MediaViewData> galleryVideos;
+	private List<MediaViewData> galleryVideos;
 
-   private Map<String, String> featureLists = new HashMap<String, String>();
+	private Map<String, String> featureLists = new HashMap<String, String>();
 
-   private List<String> names;
+	private List<String> names;
 
-   private String code;
+	private String code;
+
+	private String keyFacts;
+
+	private String imgSrc;
 
 
-   /**
-    * @return the galleryImages
-    */
-   public List<MediaViewData> getGalleryImages()
-   {
-      return galleryImages;
-   }
+	/**
+	 * @return the galleryImages
+	 */
+	public List<MediaViewData> getGalleryImages()
+	{
+		return galleryImages;
+	}
 
-   /**
-    * @param galleryImages the galleryImages to set
-    */
-   public void setGalleryImages(List<MediaViewData> galleryImages)
-   {
-      this.galleryImages = galleryImages;
-   }
+	/**
+	 * @param galleryImages
+	 *           the galleryImages to set
+	 */
+	public void setGalleryImages(final List<MediaViewData> galleryImages)
+	{
+		this.galleryImages = galleryImages;
+	}
 
-   /**
-    * @return the galleryVideos
-    */
-   public List<MediaViewData> getGalleryVideos()
-   {
-      return galleryVideos;
-   }
+	/**
+	 * @return the galleryVideos
+	 */
+	public List<MediaViewData> getGalleryVideos()
+	{
+		return galleryVideos;
+	}
 
-   /**
-    * @param galleryVideos the galleryVideos to set
-    */
-   public void setGalleryVideos(List<MediaViewData> galleryVideos)
-   {
-      this.galleryVideos = galleryVideos;
-   }
+	/**
+	 * @param galleryVideos
+	 *           the galleryVideos to set
+	 */
+	public void setGalleryVideos(final List<MediaViewData> galleryVideos)
+	{
+		this.galleryVideos = galleryVideos;
+	}
 
-   /**
-    * @return the displayName
-    */
-   public String getDisplayName()
-   {
-      return displayName;
-   }
+	/**
+	 * @return the displayName
+	 */
+	public String getDisplayName()
+	{
+		return displayName;
+	}
 
-   /**
-    * @param displayName
-    *           the displayName to set
-    */
-   public void setDisplayName(final String displayName)
-   {
-      this.displayName = displayName;
-   }
+	/**
+	 * @param displayName
+	 *           the displayName to set
+	 */
+	public void setDisplayName(final String displayName)
+	{
+		this.displayName = displayName;
+	}
 
-   /**
-    * @return the featureLists
-    */
-   public Map<String, String> getFeatureLists()
-   {
-      return featureLists;
-   }
+	/**
+	 * @return the featureLists
+	 */
+	public Map<String, String> getFeatureLists()
+	{
+		return featureLists;
+	}
 
-   /**
-    * @param featureLists the featureLists to set
-    */
-   public void setFeatureLists(Map<String, String> featureLists)
-   {
-      this.featureLists = featureLists;
-   }
+	/**
+	 * @param featureLists
+	 *           the featureLists to set
+	 */
+	public void setFeatureLists(final Map<String, String> featureLists)
+	{
+		this.featureLists = featureLists;
+	}
 
-   /**
-    * @return the names
-    */
-   public List<String> getNames()
-   {
-      return names;
-   }
+	/**
+	 * @return the names
+	 */
+	public List<String> getNames()
+	{
+		return names;
+	}
 
-   /**
-    * @param names the names to set
-    */
-   public void setNames(List<String> names)
-   {
-      this.names = names;
-   }
+	/**
+	 * @param names
+	 *           the names to set
+	 */
+	public void setNames(final List<String> names)
+	{
+		this.names = names;
+	}
 
-   /**
-    * @return the code
-    */
-   public String getCode()
-   {
-      return code;
-   }
+	/**
+	 * @return the code
+	 */
+	public String getCode()
+	{
+		return code;
+	}
 
-   /**
-    * @param code the code to set
-    */
-   public void setCode(String code)
-   {
-      this.code = code;
-   }
+	/**
+	 * @param code
+	 *           the code to set
+	 */
+	public void setCode(final String code)
+	{
+		this.code = code;
+	}
 
-   /**
-    * @return the introduction
-    */
-   public String getIntroduction()
-   {
-      return introduction;
-   }
+	/**
+	 * @return the introduction
+	 */
+	public String getIntroduction()
+	{
+		return introduction;
+	}
 
-   /**
-    * @param introduction the introduction to set
-    */
-   public void setIntroduction(String introduction)
-   {
-      this.introduction = introduction;
-   }
+	/**
+	 * @param introduction
+	 *           the introduction to set
+	 */
+	public void setIntroduction(final String introduction)
+	{
+		this.introduction = introduction;
+	}
+
+	/**
+	 * @return the keyFacts
+	 */
+	public String getKeyFacts()
+	{
+		return keyFacts;
+	}
+
+	/**
+	 * @param keyFacts
+	 *           the keyFacts to set
+	 */
+	public void setKeyFacts(final String keyFacts)
+	{
+		this.keyFacts = keyFacts;
+	}
+
+	/**
+	 * @return the imgSrc
+	 */
+	public String getImgSrc()
+	{
+		return imgSrc;
+	}
+
+	/**
+	 * @param imgSrc
+	 *           the imgSrc to set
+	 */
+	public void setImgSrc(final String imgSrc)
+	{
+		this.imgSrc = imgSrc;
+	}
 
 
 
