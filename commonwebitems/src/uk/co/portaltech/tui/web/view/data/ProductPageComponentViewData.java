cb578474c60eaaeddcf28ccaa96f4c82ff9d73b2
phanisekhar.n <phanisekhar.n>
2015-09-27 15:23:18 +0000

    Completed US24589 : Added usps to productpagecomponent view data for for flights economy club component implementation .

--- commonwebitems/src/uk/co/portaltech/tui/web/view/data/ProductPageComponentViewData.java
+++ commonwebitems/src/uk/co/portaltech/tui/web/view/data/ProductPageComponentViewData.java
@@ -3,6 +3,7 @@
  */
 package uk.co.portaltech.tui.web.view.data;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -14,405 +15,408 @@ import java.util.Map;
  */
 public class ProductPageComponentViewData
 {
-   private String featureTitle;
+	private String featureTitle;
 
-   private String name;
+	private String name;
 
-   private String imageUrl;
+	private String imageUrl;
 
-   private String description;
+	private String description;
 
-   private boolean isSubImage;
+	private boolean isSubImage;
 
-   private String featureCopy;
+	private String featureCopy;
 
-   private String thunbnailUrl;
+	private String thunbnailUrl;
 
-   private String videoUrl;
+	private String videoUrl;
 
-   private String subTitle;
+	private String subTitle;
 
-   private List<String> urlsList;
+	private List<String> urlsList;
 
-   private Map<String, String> imageUrlsMap = new HashMap<String, String>();
+	private Map<String, String> imageUrlsMap = new HashMap<String, String>();
 
-   private String blog;
+	private String blog;
 
-   private String brochure;
+	private String brochure;
 
-   private String email;
+	private String email;
 
-   private String callUsOn;
+	private String callUsOn;
 
-   private String extraDetails;
+	private String extraDetails;
 
-   private String collectionLinkUrl;
+	private String collectionLinkUrl;
 
-   private String url;
+	private String url;
 
-   private String linkText;
+	private String linkText;
 
-   private String title;
+	private String title;
 
-   private Map<String, String> usps = new HashMap<String, String>();
+	private List<String> usps = new ArrayList<String>();
 
-   /**
-    * @return the usps
-    */
-   public Map<String, String> getUsps()
-   {
-      return usps;
-   }
+	/**
+	 * @return the usps
+	 */
+	public List<String> getUsps()
+	{
+		return usps;
+	}
 
-   /**
-    * @param usps
-    *           the usps to set
-    */
-   public void setUsps(final Map<String, String> usps)
-   {
-      this.usps = usps;
-   }
-
-   /**
-    * @return the collectionLinkUrl
-    */
-   public String getCollectionLinkUrl()
-   {
-      return collectionLinkUrl;
-   }
-
-   /**
-    * @param collectionLinkUrl
-    *           the collectionLinkUrl to set
-    */
-   public void setCollectionLinkUrl(final String collectionLinkUrl)
-   {
-      this.collectionLinkUrl = collectionLinkUrl;
-   }
-
-   /**
-    * @return the urlsList
-    */
-   public List<String> getUrlsList()
-   {
-      return urlsList;
-   }
-
-   /**
-    * @param urlsList
-    *           the urlsList to set
-    */
-   public void setUrlsList(final List<String> urlsList)
-   {
-      this.urlsList = urlsList;
-   }
-
-   /**
-    * @return the subTitle
-    */
-   public String getSubTitle()
-   {
-      return subTitle;
-   }
-
-   /**
-    * @param subTitle
-    *           the subTitle to set
-    */
-   public void setSubTitle(final String subTitle)
-   {
-      this.subTitle = subTitle;
-   }
-
-   /**
-    * @return the thunbnailUrl
-    */
-   public String getThunbnailUrl()
-   {
-      return thunbnailUrl;
-   }
-
-   /**
-    * @param thunbnailUrl
-    *           the thunbnailUrl to set
-    */
-   public void setThunbnailUrl(final String thunbnailUrl)
-   {
-      this.thunbnailUrl = thunbnailUrl;
-   }
-
-   /**
-    * @return the featureCopy
-    */
-   public String getFeatureCopy()
-   {
-      return featureCopy;
-   }
-
-   /**
-    * @param featureCopy
-    *           the featureCopy to set
-    */
-   public void setFeatureCopy(final String featureCopy)
-   {
-      this.featureCopy = featureCopy;
-   }
-
-   /**
-    * @return the title
-    */
-   public String getTitle()
-   {
-      return title;
-   }
-
-   /**
-    * @param title
-    *           the title to set
-    */
-   public void setTitle(final String title)
-   {
-      this.title = title;
-   }
-
-   /**
-    * @return the videoUrl
-    */
-   public String getVideoUrl()
-   {
-      return videoUrl;
-   }
-
-   /**
-    * @param videoUrl
-    *           the videoUrl to set
-    */
-   public void setVideoUrl(final String videoUrl)
-   {
-      this.videoUrl = videoUrl;
-   }
+	/**
+	 * @param usps
+	 *           the usps to set
+	 */
+	public void setUsps(final List<String> usps)
+	{
+		this.usps = usps;
+	}
 
 
-   /**
-    *
-    * @return the featureTitle
-    */
-   public String getFeatureTitle()
-   {
-      return featureTitle;
-   }
 
-   /**
-    * @param featureTitle
-    *           the featureTitle to set
-    */
-   public void setFeatureTitle(final String featureTitle)
-   {
-      this.featureTitle = featureTitle;
-   }
 
-   /**
-    * @return the name
-    */
-   public String getName()
-   {
-      return name;
-   }
+	/**
+	 * @return the collectionLinkUrl
+	 */
+	public String getCollectionLinkUrl()
+	{
+		return collectionLinkUrl;
+	}
 
-   /**
-    * @param name
-    *           the name to set
-    */
-   public void setName(final String name)
-   {
-      this.name = name;
-   }
+	/**
+	 * @param collectionLinkUrl
+	 *           the collectionLinkUrl to set
+	 */
+	public void setCollectionLinkUrl(final String collectionLinkUrl)
+	{
+		this.collectionLinkUrl = collectionLinkUrl;
+	}
 
-   /**
-    * @return the imageUrl
-    */
-   public String getImageUrl()
-   {
-      return imageUrl;
-   }
+	/**
+	 * @return the urlsList
+	 */
+	public List<String> getUrlsList()
+	{
+		return urlsList;
+	}
 
-   /**
-    * @param imageUrl
-    *           the imageUrl to set
-    */
-   public void setImageUrl(final String imageUrl)
-   {
-      this.imageUrl = imageUrl;
-   }
+	/**
+	 * @param urlsList
+	 *           the urlsList to set
+	 */
+	public void setUrlsList(final List<String> urlsList)
+	{
+		this.urlsList = urlsList;
+	}
 
-   /**
-    * @return the description
-    */
-   public String getDescription()
-   {
-      return description;
-   }
+	/**
+	 * @return the subTitle
+	 */
+	public String getSubTitle()
+	{
+		return subTitle;
+	}
 
-   /**
-    * @param description
-    *           the description to set
-    */
-   public void setDescription(final String description)
-   {
-      this.description = description;
-   }
+	/**
+	 * @param subTitle
+	 *           the subTitle to set
+	 */
+	public void setSubTitle(final String subTitle)
+	{
+		this.subTitle = subTitle;
+	}
 
-   /**
-    * @return the isSubImage
-    */
-   public boolean isSubImage()
-   {
-      return isSubImage;
-   }
+	/**
+	 * @return the thunbnailUrl
+	 */
+	public String getThunbnailUrl()
+	{
+		return thunbnailUrl;
+	}
 
-   /**
-    * @param isSubImage
-    *           the isSubImage to set
-    */
-   public void setSubImage(final boolean isSubImage)
-   {
-      this.isSubImage = isSubImage;
-   }
+	/**
+	 * @param thunbnailUrl
+	 *           the thunbnailUrl to set
+	 */
+	public void setThunbnailUrl(final String thunbnailUrl)
+	{
+		this.thunbnailUrl = thunbnailUrl;
+	}
 
-   /**
-    * @return the blog
-    */
-   public String getBlog()
-   {
-      return blog;
-   }
+	/**
+	 * @return the featureCopy
+	 */
+	public String getFeatureCopy()
+	{
+		return featureCopy;
+	}
 
-   /**
-    * @param blog
-    *           the blog to set
-    */
-   public void setBlog(final String blog)
-   {
-      this.blog = blog;
-   }
+	/**
+	 * @param featureCopy
+	 *           the featureCopy to set
+	 */
+	public void setFeatureCopy(final String featureCopy)
+	{
+		this.featureCopy = featureCopy;
+	}
 
-   /**
-    * @return the brochure
-    */
-   public String getBrochure()
-   {
-      return brochure;
-   }
+	/**
+	 * @return the title
+	 */
+	public String getTitle()
+	{
+		return title;
+	}
 
-   /**
-    * @param brochure
-    *           the brochure to set
-    */
-   public void setBrochure(final String brochure)
-   {
-      this.brochure = brochure;
-   }
+	/**
+	 * @param title
+	 *           the title to set
+	 */
+	public void setTitle(final String title)
+	{
+		this.title = title;
+	}
 
-   /**
-    * @return the email
-    */
-   public String getEmail()
-   {
-      return email;
-   }
+	/**
+	 * @return the videoUrl
+	 */
+	public String getVideoUrl()
+	{
+		return videoUrl;
+	}
 
-   /**
-    * @param email
-    *           the email to set
-    */
-   public void setEmail(final String email)
-   {
-      this.email = email;
-   }
+	/**
+	 * @param videoUrl
+	 *           the videoUrl to set
+	 */
+	public void setVideoUrl(final String videoUrl)
+	{
+		this.videoUrl = videoUrl;
+	}
 
-   /**
-    * @return the callUsOn
-    */
-   public String getCallUsOn()
-   {
-      return callUsOn;
-   }
 
-   /**
-    * @param callUsOn
-    *           the callUsOn to set
-    */
-   public void setCallUsOn(final String callUsOn)
-   {
-      this.callUsOn = callUsOn;
-   }
+	/**
+	 *
+	 * @return the featureTitle
+	 */
+	public String getFeatureTitle()
+	{
+		return featureTitle;
+	}
 
-   /**
-    * @return the extraDetails
-    */
-   public String getExtraDetails()
-   {
-      return extraDetails;
-   }
+	/**
+	 * @param featureTitle
+	 *           the featureTitle to set
+	 */
+	public void setFeatureTitle(final String featureTitle)
+	{
+		this.featureTitle = featureTitle;
+	}
 
-   /**
-    * @param extraDetails
-    *           the extraDetails to set
-    */
-   public void setExtraDetails(final String extraDetails)
-   {
-      this.extraDetails = extraDetails;
-   }
+	/**
+	 * @return the name
+	 */
+	public String getName()
+	{
+		return name;
+	}
 
-   /**
-    * @return the imageUrlsMap
-    */
-   public Map<String, String> getImageUrlsMap()
-   {
-      return imageUrlsMap;
-   }
+	/**
+	 * @param name
+	 *           the name to set
+	 */
+	public void setName(final String name)
+	{
+		this.name = name;
+	}
 
-   /**
-    * @param imageUrlsMap
-    *           the imageUrlsMap to set
-    */
-   public void setImageUrlsMap(final Map<String, String> imageUrlsMap)
-   {
-      this.imageUrlsMap = imageUrlsMap;
-   }
+	/**
+	 * @return the imageUrl
+	 */
+	public String getImageUrl()
+	{
+		return imageUrl;
+	}
 
-   /**
-    * @return the url
-    */
-   public String getUrl()
-   {
-      return url;
-   }
+	/**
+	 * @param imageUrl
+	 *           the imageUrl to set
+	 */
+	public void setImageUrl(final String imageUrl)
+	{
+		this.imageUrl = imageUrl;
+	}
 
-   /**
-    * @param url
-    *           the url to set
-    */
-   public void setUrl(final String url)
-   {
-      this.url = url;
-   }
+	/**
+	 * @return the description
+	 */
+	public String getDescription()
+	{
+		return description;
+	}
 
-   /**
-    * @return the linkText
-    */
-   public String getLinkText()
-   {
-      return linkText;
-   }
+	/**
+	 * @param description
+	 *           the description to set
+	 */
+	public void setDescription(final String description)
+	{
+		this.description = description;
+	}
 
-   /**
-    * @param linkText
-    *           the linkText to set
-    */
-   public void setLinkText(final String linkText)
-   {
-      this.linkText = linkText;
-   }
+	/**
+	 * @return the isSubImage
+	 */
+	public boolean isSubImage()
+	{
+		return isSubImage;
+	}
+
+	/**
+	 * @param isSubImage
+	 *           the isSubImage to set
+	 */
+	public void setSubImage(final boolean isSubImage)
+	{
+		this.isSubImage = isSubImage;
+	}
+
+	/**
+	 * @return the blog
+	 */
+	public String getBlog()
+	{
+		return blog;
+	}
+
+	/**
+	 * @param blog
+	 *           the blog to set
+	 */
+	public void setBlog(final String blog)
+	{
+		this.blog = blog;
+	}
+
+	/**
+	 * @return the brochure
+	 */
+	public String getBrochure()
+	{
+		return brochure;
+	}
+
+	/**
+	 * @param brochure
+	 *           the brochure to set
+	 */
+	public void setBrochure(final String brochure)
+	{
+		this.brochure = brochure;
+	}
+
+	/**
+	 * @return the email
+	 */
+	public String getEmail()
+	{
+		return email;
+	}
+
+	/**
+	 * @param email
+	 *           the email to set
+	 */
+	public void setEmail(final String email)
+	{
+		this.email = email;
+	}
+
+	/**
+	 * @return the callUsOn
+	 */
+	public String getCallUsOn()
+	{
+		return callUsOn;
+	}
+
+	/**
+	 * @param callUsOn
+	 *           the callUsOn to set
+	 */
+	public void setCallUsOn(final String callUsOn)
+	{
+		this.callUsOn = callUsOn;
+	}
+
+	/**
+	 * @return the extraDetails
+	 */
+	public String getExtraDetails()
+	{
+		return extraDetails;
+	}
+
+	/**
+	 * @param extraDetails
+	 *           the extraDetails to set
+	 */
+	public void setExtraDetails(final String extraDetails)
+	{
+		this.extraDetails = extraDetails;
+	}
+
+	/**
+	 * @return the imageUrlsMap
+	 */
+	public Map<String, String> getImageUrlsMap()
+	{
+		return imageUrlsMap;
+	}
+
+	/**
+	 * @param imageUrlsMap
+	 *           the imageUrlsMap to set
+	 */
+	public void setImageUrlsMap(final Map<String, String> imageUrlsMap)
+	{
+		this.imageUrlsMap = imageUrlsMap;
+	}
+
+	/**
+	 * @return the url
+	 */
+	public String getUrl()
+	{
+		return url;
+	}
+
+	/**
+	 * @param url
+	 *           the url to set
+	 */
+	public void setUrl(final String url)
+	{
+		this.url = url;
+	}
+
+	/**
+	 * @return the linkText
+	 */
+	public String getLinkText()
+	{
+		return linkText;
+	}
+
+	/**
+	 * @param linkText
+	 *           the linkText to set
+	 */
+	public void setLinkText(final String linkText)
+	{
+		this.linkText = linkText;
+	}
 
 }
